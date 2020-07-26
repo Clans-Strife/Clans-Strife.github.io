6fbdd436f4df0acad7e50f3a2a6942137165bcb3
Clans-Strife <68481424+Clans-Strife@users.noreply.github.com>
2020-07-26 09:23:14 +0300

    1

---
+++ Shop/Пример Script.js
@@ -0,0 +1,6 @@
+a="background-color:#00ffff; color:#ff00ff; font-size:24pt; font-family:'Times New Roman'" 
+naim='Сеть магазинов "ВСЁ ДЛЯ ДОМА"' 
+var da=new Date() 
+var d=da.getDate()+"."+da.getMonth()+"."+da.getYear() 
+document.write('<P align=center style= "'+a+'">'+ naim+'</p><P>Сегодня '+d+'</p>')
+
